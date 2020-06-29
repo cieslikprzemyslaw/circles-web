7deddd025a61506f655f39746252f0ac00d07247
xomod <xomod99@gmail.com>
2020-06-29 23:53:05 +0200

    :sparkles: Implement room creating

---
+++ src/components/common/AccountPicker/index.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import useStyles from "./styles";
+import { useCommand } from "api/hooks";
+import { AccountList, AccountInfo } from "api/commands";
+import { useForkedState } from "utils/hooks/general";
+import { isLoaded } from "api/utils";
+import { TContact } from "types";
+import { CircularProgress } from "@material-ui/core";
+import { useStore } from "store/hooks";
+
+type AccountPickerProps = {
+    onChange: React.Dispatch<React.SetStateAction<string[]>> | React.Dispatch<React.SetStateAction<string>>;
+    value: string | string[]
+    multiple?: boolean;
+}
+
+const AccountPicker = (props: AccountPickerProps) => {
+    const classes = useStyles();
+
+    const currentAccount = useStore(state => state.currentAccount);
+
+    const handleChange = (account_id: string) => () => {
+        // @ts-ignore
+        props.onChange && props.onChange((value) => {
+            if(props.multiple && Array.isArray(value)) {
+                if(value.includes(account_id)) return [...value].splice(value.indexOf(account_id), 1);
+                return [...value, account_id];
+            } else {
+                return value === account_id ? "" : account_id;
+            }
+        });
+    }
+
+    if(!currentAccount?.contacts) return <CircularProgress />
+
+    return <div className={classes.root}>
+        {
+            currentAccount?.contacts?.map((contact: TContact) => 
+                <SingleAccount key={contact.account_id} contact={contact} onClick={handleChange(contact.account_id)} selected={props.value.includes(contact.account_id)} />)
+        }
+    </div>
+}
+
+const SingleAccount = ({ contact, onClick, selected }: { contact: TContact, onClick: () => void, selected: boolean }) => {
+    const accountRq = useCommand(AccountInfo, contact.account_id);
+    const [account] = useForkedState(rq => isLoaded(rq) ? rq.data : null, accountRq)
+
+    if(!account) return <div style={{fontSize: "24px"}}>Loading...</div>;
+
+    return <div onClick={onClick} style={{color: selected ? "#449ee2" : "black", cursor: "pointer", fontSize: "24px"}}>{account.label}</div>
+}
+
+export default AccountPicker;
---
+++ src/components/pages/Home/CreateRoom.tsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Button, Dialog, DialogTitle, DialogContent, Typography, DialogActions, TextField } from '@material-ui/core';
+import AccountPicker from 'components/common/AccountPicker';
+import { useHistory } from 'react-router-dom';
+import { useStore } from 'store/hooks';
+import { useDispatchCommand } from 'api/hooks';
+import { RoomCreate } from 'api/commands';
+
+
+const CreateRoom = () => {
+
+    const [open, setOpen] = React.useState(false);
+    const [selected, setSelected] = React.useState<string[]>([]);
+    const [label, setLabel] = React.useState("");
+  
+    const history = useHistory();
+    const currentAccount = useStore(state => state.currentAccount);
+    const dispatchCommand = useDispatchCommand();
+
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    const handleSubmit = async () => {
+      const newRoom = await dispatchCommand(RoomCreate, currentAccount?.id ?? "", label, selected);
+
+      if (newRoom.status === 200) {
+          const room_id = newRoom!.data!.room_id;
+          history.push(`/room/${room_id}`)
+      } else if(newRoom.status === 404) {
+          // TODO: Error while creating room, error handling.. :( 
+      }
+    }
+
+    const handleLabelChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      // @ts-ignore
+      const value = event?.target?.value ?? "";
+
+      setLabel(value);
+    }
+
+    return <div>
+        <Button variant="contained" onClick={handleClickOpen}>Create Room</Button>
+        
+        <Dialog fullWidth onClose={handleClose} aria-labelledby="customized-dialog-title" open={open}>
+        <DialogTitle id="customized-dialog-title">
+          Create room
+        </DialogTitle>
+        
+        <DialogContent dividers>
+          <TextField fullWidth id="outlined-basic" label="Room name" value={label} onChange={handleLabelChange} />
+            
+           
+          <div style={{paddingTop: "20px"}}><AccountPicker multiple value={selected} onChange={setSelected} /></div>
+        </DialogContent>
+        
+        <DialogActions>
+          <Button autoFocus onClick={handleClose} color="primary">
+            Close
+          </Button>
+          <Button autoFocus onClick={handleSubmit} color="primary">
+            Create
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+
+
+}
+
+
+
+export default CreateRoom;
