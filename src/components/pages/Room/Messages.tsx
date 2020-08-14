f35ad8330cb7972a82ed7a56913463aa5e6f5a86
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-08-14 21:15:20 +0100

    deleted unexpected line of code

--- src/components/pages/Room/Messages.tsx
+++ src/components/pages/Room/Messages.tsx
@@ -22,7 +22,7 @@ const Messages = ({ roomId, accounts }: { roomId: string, accounts: IAccount[] }
 
     if (Object.keys(messages).length === 0) return <Typography variant="h2">Say Hi!</Typography>
 
-    return <Flex flexDirection="column-reverse" className={messagesClasses}>
+    return <Flex flexDirection="column-reverse">
         {
             Object.keys(messages).reverse().map((message_id: string) => {
 
