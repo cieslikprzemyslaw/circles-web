a81348c0934a4c9308cf7bdbc0a6a6fe9a2bf5b0
cieslikprzemyslaw <cprzemek91@hotmail.com>
2020-08-15 21:50:19 +0100

    correct margin for ownMessage

--- src/components/pages/Room/Messages.tsx
+++ src/components/pages/Room/Messages.tsx
@@ -11,7 +11,6 @@ import ReactMarkdown from "react-markdown";
 
 
 const Messages = ({ roomId, accounts }: { roomId: string, accounts: IAccount[] }) => {
-    const classes = useStyles();
     const messages = useMessages(roomId);
 
 
@@ -56,7 +55,7 @@ const Message = ({ message, author, isOwned }: { message: IMessage, author: IAcc
         setHeight((ref as any).current.offsetHeight);
     }, [])
 
-    return <Flex alignItems="flex-end" className={isOwned ? classes.ownedMessage : classes.messageRoot} style={{ alignSelf: isOwned ? "flex-end" : "flex-start", margin: height > 150 ? "8px":"15px" }}>
+    return <Flex alignItems="flex-end" className={isOwned ? classes.ownedMessage : classes.messageRoot} style={{ alignSelf: isOwned ? "flex-end" : "flex-start", margin: height > 200 ? "8px":null }}>
         {!isOwned && author?.avatar_url && <img alt="author avatar" src={author.avatar_url} className={classes.avatar} />}
         <div className={rootClasses} ref={ref}>
             <ReactMarkdown source={value} />
