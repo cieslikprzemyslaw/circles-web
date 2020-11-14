c60fdc554a4cb9e54ef045e7c616e910427f3dd9
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2020-11-14 12:58:21 +0000

    :lipstick: deleete empty section

--- src/components/pages/Highlights/index.tsx
+++ src/components/pages/Highlights/index.tsx
@@ -14,7 +14,6 @@ const Highlights = () => {
   const [translated] = useLittera(translations);
   const classes = useStyles();
 
-
   const currentAccount = useStore(state => state.currentAccount);
 
   return (
@@ -25,9 +24,6 @@ const Highlights = () => {
       </section>
       <section className={classes.avatarContainer}>
           <img className={classes.avatar} src={currentAccount?.avatar_url ? currentAccount?.avatar_url : `https://eu.ui-avatars.com/api/?name=${currentAccount?.name}`} alt="account avatar" onClick={() => history.push("/profile")}/> 
-      </section>
-      <section>
-        
       </section>
     </div>
   );
