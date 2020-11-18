ce0758e27d69e68f3c0c2090d25432ab92c32656
Przemyslaw Cieslik <cprzemek91@hotmail.com>
2020-11-18 21:07:39 +0000

    :lipstick: unifinished part of code

--- src/components/common/AccountSugestions/index.tsx
+++ src/components/common/AccountSugestions/index.tsx
@@ -3,23 +3,31 @@ import { AccountGetSuggestions } from 'api/commands';
 import { useCommand } from 'api/hooks';
 import React from 'react';
 import { useStore } from 'store/hooks';
+import { useForkedState } from "utils/hooks/general";
+import { isLoaded } from "api/utils";
 
 const AccountSuggestions = () => {
     const currentAccount = useStore(state => state.currentAccount ?? null);
 
-    const getSuggestions = useCommand(AccountGetSuggestions)
-    console.log(currentAccount)
+    const suggestionsRq = useCommand(AccountGetSuggestions, currentAccount?.id)
+    const [suggestions] = useForkedState(rq => isLoaded(rq) ? rq.data : null, suggestionsRq);
+    
+    console.log(suggestions)
 
     return (
-        <Card>
-            <div>
-                <CardContent>
-                    <Typography component="h4" variant="h4">
-
-                    </Typography>
-                </CardContent>
-            </div>
-        </Card>
+           <>
+           {suggestions?.map((suggestion:any) => {
+               return (
+                <Card>
+                    <div>
+                        <CardContent>
+                            
+                        </CardContent>
+                    </div>
+                </Card>
+               )
+           })}
+           </>
     );
 };
 
