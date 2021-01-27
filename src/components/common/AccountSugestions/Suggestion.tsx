311eaea02788f1351d75f93e177c3ee4d655bd2f
Mike Eling <mike.eling97@gmail.com>
2021-01-27 02:43:01 +0100

    :art: Add avatars for suggestions

--- src/components/common/AccountSugestions/Suggestion.tsx
+++ src/components/common/AccountSugestions/Suggestion.tsx
@@ -3,13 +3,14 @@ import { AccountInfo } from 'api/commands';
 import { useForkedState } from "utils/hooks/general";
 import { isLoaded } from "api/utils";
 import { useCommand } from 'api/hooks';
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Card, CardContent, Typography, Box, Avatar } from '@material-ui/core';
 import translations from "./trans";
 import { useLittera } from "react-littera";
 import { IAccount, TSuggestion } from "types";
 import useStyles from "./styles";
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
+import { makeFullName, makeInitials } from 'utils/general';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -27,17 +28,26 @@ const Suggestion = ({suggestion, index}: {suggestion: TSuggestion, index: string
         
     }, [suggestion])
 
-    if (!account) return null; // TODO: Add a loader...
+    if (!account || account.flags?.includes("needs_init")) return null; // TODO: Add a loader...
+
+    const accountFullName = makeFullName(account.details?.first_name, account.details?.last_name, account.label);
+    const accountInitials = makeInitials(account.details?.first_name, account.details?.last_name, account.label);
 
     return (
         <Card className={classes.card} id={index} ref={scrollRef} style={{zIndex: parseInt(index)}}>
                 <CardContent>
-                <Typography variant="h5">
-                    {`${account.details.first_name} ${account.details.last_name}` ?? account.label}
+                <Box display="flex" alignItems="center" justifyContent="flex-start">
+                    <Avatar className={classes.suggestionAvatar} src={account.avatar_url} alt={accountFullName} >{accountInitials}</Avatar>
+                    <Box>
+                        <Typography variant="h5">
+                            {accountFullName}
                         </Typography>
-                <Typography>
-                        {type === "never-messaged" ? translated.firstMessage : type}
-                    </Typography>
+
+                        <Typography>
+                            {type === "never-messaged" ? translated.firstMessage : type}
+                        </Typography>
+                    </Box>
+                </Box>
                 </CardContent>
             </Card>
     );
